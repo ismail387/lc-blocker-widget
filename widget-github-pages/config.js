c03bfc030ca0b7f0962adae5a3c615b10cbce631
ismail387 <ismail@giti2c.com>
2026-09-25 15:59:42 +0200

    Update LiveChat widget configuration values

--- widget-github-pages/config.js
+++ widget-github-pages/config.js
@@ -1,7 +1,7 @@
 // Fill these two values, then publish the widget folder.
 window.LCB_CONFIG = {
   // Client ID of the NEW widget app in LiveChat Developer Console (not the webhook app)
-  clientId: 'WIDGET_APP_CLIENT_ID',
+  clientId: '5e7fca15e04eda0dcdf07283c6d2d8b5',
   // Web app URL of the Admin API Apps Script project (ends with /exec)
-  apiUrl: 'ADMIN_API_URL'
+  apiUrl: 'https://script.google.com/macros/s/AKfycbyGoXM81Oliidx-nTQv4RiLTsMu-3MJU-4jp6UJ1GxAiW0MzwsIJMkzICt9oRiEHTpZ/exec'
 };
